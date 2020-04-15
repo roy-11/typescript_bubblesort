e7b80885add2e488c42dc49c8110b3ff5ab948b4
ryosukehagiwara <hukurahagi1109@gmail.com>
2020-04-15 16:57:41 +0900

    add sort logic for LinkedList

---
+++ src/LinkedList.ts
@@ -0,0 +1,83 @@
+class Node {
+  next: Node | null = null;
+  constructor(public data: number) {}
+}
+
+export class LinkedList {
+  head: Node | null = null;
+
+  add(data: number): void {
+    if (!this.head) {
+      this.head = new Node(data);
+      return;
+    }
+
+    let tail = this.head;
+    while (tail.next) {
+      tail = tail.next;
+    }
+
+    tail.next = new Node(data);
+  }
+
+  at(index: number): Node {
+    if (!this.head) {
+      throw new Error("LinkedList doesn't have the index");
+    }
+
+    let counter = 0;
+    let node: Node | null = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+
+      counter++;
+      node = node.next;
+    }
+    throw new Error("LinkedList doesn't have the index");
+  }
+
+  get length(): number {
+    if (!this.head) {
+      throw 0;
+    }
+
+    let length = 1;
+    let node = this.head;
+    while (node.next) {
+      length++;
+      node = node.next;
+    }
+    return length;
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    if (!this.head) {
+      throw new Error("LinkedList has no Node");
+    }
+
+    const leftNode = this.at(leftIndex);
+    const rightNode = this.at(rightIndex);
+    return leftNode.data > rightNode.data;
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftNode = this.at(leftIndex);
+    const rightNode = this.at(rightIndex);
+
+    const leftHand = leftNode.data;
+    leftNode.data = rightNode.data;
+    rightNode.data = leftHand;
+  }
+
+  print(): void {
+    if (!this.head) return;
+
+    let node: Node | null = this.head;
+    while (node) {
+      console.log(node.data);
+      node = node.next;
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 import { Sorter } from "./Sorter";
 
 // Number
@@ -13,3 +14,15 @@ const charactersCollection = new CharactersCollection("EaXp");
 const sorter2 = new Sorter(charactersCollection);
 sorter2.sort();
 console.log(sorter2.collection);
+
+// LinkedList
+const linkedList = new LinkedList();
+linkedList.add(1);
+linkedList.add(4);
+linkedList.add(2);
+linkedList.add(8);
+linkedList.add(-1);
+linkedList.add(-10);
+const sorter3 = new Sorter(linkedList);
+sorter3.sort();
+linkedList.print();
