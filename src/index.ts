34355d6fb186ea1a171c0cc5f8a3eeccb15efd11
ryosukehagiwara <hukurahagi1109@gmail.com>
2020-04-15 14:46:28 +0900

    add sort logic for characters

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 import { Sorter } from "./Sorter";
 
+// Number
 const numbersCollection = new NumbersCollection([1, 4, 2, 8, -1, -10]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
-console.log(sorter.collection);
+const sorter1 = new Sorter(numbersCollection);
+sorter1.sort();
+console.log(sorter1.collection);
+
+// String
+const charactersCollection = new CharactersCollection("EaXp");
+const sorter2 = new Sorter(charactersCollection);
+sorter2.sort();
+console.log(sorter2.collection);
