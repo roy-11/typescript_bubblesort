a2c66286b5890e5d4e9ff73b6afcafeaa69aa5c6
ryosukehagiwara <hukurahagi1109@gmail.com>
2020-04-16 13:48:02 +0900

    change Sort Class into Abstract Class

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,16 @@
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
-import { Sorter } from "./Sorter";
 
 // Number
 const numbersCollection = new NumbersCollection([1, 4, 2, 8, -1, -10]);
-const sorter1 = new Sorter(numbersCollection);
-sorter1.sort();
-console.log(sorter1.collection);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
-// String
+// String;
 const charactersCollection = new CharactersCollection("EaXp");
-const sorter2 = new Sorter(charactersCollection);
-sorter2.sort();
-console.log(sorter2.collection);
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
 // LinkedList
 const linkedList = new LinkedList();
@@ -23,6 +20,5 @@ linkedList.add(2);
 linkedList.add(8);
 linkedList.add(-1);
 linkedList.add(-10);
-const sorter3 = new Sorter(linkedList);
-sorter3.sort();
+linkedList.sort();
 linkedList.print();
